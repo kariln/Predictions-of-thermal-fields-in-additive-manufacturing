02ae284e0ded3d6f0effcd8044c25106c0124dcd
Kari Lovise Ness <42612272+kariln@users.noreply.github.com>
2020-09-24 12:30:30 +0200

    .

--- Abaqus/disk/get_odb.py
+++ Abaqus/disk/get_odb.py
@@ -15,12 +15,14 @@ odb = openOdb('Job-1.odb')
 
 #Access the steps in the odb
 step1 = odb.steps['heat']
-print('Processing step:', step1.name)
-# Edit the line below to enter the correct node number
-region = step1.historyRegions['Node corner-1.nn']
-temperatureData = region.historyOutputs['T'].data
-nn = len(temperatureData)
-dispFile = open('disp.csv','w')
-for i in range(0, nn):
-    dispFile.write('%10.4E,%10.4E \n’%(temperatureData[i][0], temperatureData[i][1])')
-dispFile.close()
+
+# Extracts a history output region for step1
+historyRegions = step1.historyRegions
+print(historyRegions)
+# region = step1.historyRegions['Node corner-1.nn']
+# temperatureData = region.historyOutputs['T'].data
+# nn = len(temperatureData)
+# dispFile = open('disp.csv','w')
+# for i in range(0, nn):
+#     dispFile.write('%10.4E,%10.4E \n’%(temperatureData[i][0], temperatureData[i][1])')
+# dispFile.close()
