6f2cd619e4a3479ffb2a1ec494ab8a336bd487f5
Kari Lovise Ness <42612272+kariln@users.noreply.github.com>
2020-09-30 16:06:21 +0200

    .

--- Deposition_Patterns/zigzag.py
+++ Deposition_Patterns/zigzag.py
@@ -58,68 +58,6 @@ class Zigzag(pattern.Pattern):
           coord[self.get_transverse_dir()] = start[self.get_transverse_dir()]
           coord[self.get_stack_dir()] = self.get_thickness() + coord[self.get_stack_dir()]
         return path
-
-    # def generate_heat_path(self):
-    #   #setting the start coordinate of the raster
-    #   coord = self.get_print_coord(0)
-      
-    #   #initial conditions:
-    #   start = copy.deepcopy(coord)
-    #   time = 0
-    #   direction = 1 #defines if the deposition moves forward or backwards
-      
-    #   layers = self.get_layer_nr()
-    #   passes = self.nr_passes()
-    #   pass_time = self.pass_time()
-    #   up_time = self.up_time()
-    #   P = self.get_power()
-      
-    #   #creating text files for heat and material path
-    #   heat_path = open("heat_path.txt","w+")
-         
-    #   for i in range(0,int(layers)):
-    #       for j in range(0,int(passes)):
-    #           heat_path.write(self.coord_string(time,coord[0],coord[1], coord[2], P))
-    #           coord[self.get_deposition_dir()] += direction*self.get_length()[self.get_deposition_dir()]
-    #           direction = direction*(-1)
-    #           time += pass_time
-    #           heat_path.write(self.coord_string(time,coord[0],coord[1],coord[2],0))
-    #           coord[self.get_transverse_dir()] += self.get_road_width()
-    #           time += up_time
-    #       coord[self.get_deposition_dir()] = start[self.get_deposition_dir()]
-    #       coord[self.get_transverse_dir()] = start[self.get_transverse_dir()]
-    #       coord[self.get_stack_dir()] += self.get_thickness()
-          
-    # def generate_material_path(self):
-    #   #setting the start coordinate of the raster
-    #   coord = self.get_print_coord(1)
-      
-    #   #initial conditions:
-    #   start = copy.deepcopy(coord)
-    #   time = 0
-    #   direction = 1 #defines if the deposition moves forward or backwards
-      
-    #   layers = self.get_layer_nr()
-    #   passes = self.nr_passes()
-    #   pass_time = self.pass_time()
-    #   up_time = self.up_time()
-    #   A = self.get_area()
-
-    #   material_path = open("material_path.txt","w+")
-         
-    #   for i in range(0,int(layers)):
-    #       for j in range(0,int(passes)):
-    #           material_path.write(self.coord_string(time,coord[0],coord[1], coord[2], A))
-    #           coord[0] += direction*self.get_x_length()
-    #           direction = direction*(-1)
-    #           time += pass_time
-    #           material_path.write(self.coord_string(time,coord[0],coord[1],coord[2],0))
-    #           coord[1] += self.get_road_width()
-    #           time += up_time
-    #       coord[1] = start[1]
-    #       coord[0] = start[0]
-    #       coord[2] += self.get_thickness()
-
                 
 def main():        
     zigzag = Zigzag(0.06, 0.01, 0.06, 0.06, -0.03, -0.03, 0.02, 0.01,5000)
